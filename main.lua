b2dc1dc8eba3279d719f5545d1ea7a7a70f3151f
ragsist <maxontopbusiness@gmail.com>
2024-10-24 04:52:18 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
--- Spectral Husk
--- The Silent Highlight that LURKS in DEATHLY QUIET
+-- Spectral Herald
+-- The Highlight that CRIES its own READINESS 
 
 local function addHighlight(model)
   local highlight = Instance.new("Highlight", model)
@@ -12,7 +12,7 @@ end
 
 local function ESP()
   for _, player in ipairs(game.Players:GetPlayers()) do
-    if player ~= game.Players.LocalPlayer then 
+    if player ~= game.Players.LocalPlayer then
       addHighlight(player.Character)
     end
   end
@@ -27,3 +27,5 @@ end)
 for _, player in ipairs(game.Players:GetPlayers()) do
   addHighlight(player.Character)
 end
+
+print("ready") -- It HERALDS its own readiness!
